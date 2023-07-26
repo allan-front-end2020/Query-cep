13c619ebad6cb4e5cfebd41c4e6a8b5f29310023
Allan Tavares <allan.tavares@globalhitss.com.br>
2023-07-25 22:37:42 -0300

    feat(Initial structure): Initial structure of the project made

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import  Form from "./components/Form/index"
+
+function App() {
+ 
+  return (
+    <div className='container'>
+      <h2>Buscador de Cep</h2>
+      <Form />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Form/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function form() {
+  return (
+    <div>
+      <h1>zzzzzzzzz</h1>
+    </div>
+  );
+}
+
+export default form;
